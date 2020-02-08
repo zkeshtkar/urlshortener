d3819e78c637c9ff8cd079206adedf24d26e352f
Zahra <zkeshtkarz@gmail.com>
2020-02-08 16:20:16 +0330

    I refactore my code and change some thing

--- src/Controllers/ConvertToShort.cs
+++ src/Controllers/ConvertToShort.cs
@@ -1,76 +1,61 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using UrlShortener.Models;
 
-namespace UrlShortener.Controllers
-{
+namespace UrlShortener.Controllers {
 
-    [Route("converting")]
+    [Route ("urls")]
     [ApiController]
-    public class ConvertToShort : Controller
-    {
-     private readonly AppDbContext _appDbContext;
-    public bool IsNotValid(string url)
-    {
-        Uri validatedUri;
+    public class ConvertToShort : Controller {
+        private readonly AppDbContext _appDbContext;
+        public bool IsNotValid (string url) {
+            Uri validatedUri;
 
-        if (Uri.TryCreate(url, UriKind.Absolute, out validatedUri)) 
-        {
-            return !(validatedUri.Scheme == Uri.UriSchemeHttp || validatedUri.Scheme == Uri.UriSchemeHttps);
+            if (Uri.TryCreate (url, UriKind.Absolute, out validatedUri)) {
+                return !(validatedUri.Scheme == Uri.UriSchemeHttp || validatedUri.Scheme == Uri.UriSchemeHttps);
+            }
+            return true;
         }
-        return true;
-    }
-        
-         
-        
-         public ConvertToShort(AppDbContext app)
-         {
-             _appDbContext=app;
-         }
-        
+
+        public ConvertToShort (AppDbContext app) {
+            _appDbContext = app;
+        }
 
         [HttpPost]
-        public ActionResult Post([FromBody]Url url)
-        {
-            var alphabets  ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            var random=new Random();
-            
-            if(IsNotValid(url.LongUrl))
-            {
-                 return BadRequest();
-            }
-            else
-            {
-                 do
-            {
-            url.ShortUrl = "";
-             for(int i=0;i<8;i++)
-             {
-                 url.ShortUrl+=alphabets[random.Next(42)];
-             }
-            }while(_appDbContext.Find(url.GetType(),url.ShortUrl)  !=null);
-            _appDbContext.urls.Add(url);
-            
-            _appDbContext.SaveChanges();
+        public ActionResult Post ([FromBody] Url url) {
+            var alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var random = new Random ();
 
-            Response r=new Response(url.ShortUrl);
-            
-            return Ok(r);
+            if (IsNotValid (url.LongUrl)) {
+                return BadRequest ();
+            } else {
+                do {
+                    url.ShortUrl = "";
+                    for (int i = 0; i < 8; i++) {
+                        url.ShortUrl += alphabets[random.Next (42)];
+                    }
+                } while (_appDbContext.Find (url.GetType (), url.ShortUrl) != null);
+
+                _appDbContext.urls.Add (url);
+
+                _appDbContext.SaveChanges ();
+
+                Response r = new Response (url.ShortUrl);
+
+                return Ok (r);
             }
         }
-        
+
     }
-    
-    class Response
-    {
-        public string ShortUrl{get;set;}
-        public Response(string shortUrl)
-        {
-            this.ShortUrl=shortUrl;
+
+    class Response {
+        public string ShortUrl { get; set; }
+        public Response (string shortUrl) {
+            this.ShortUrl = shortUrl;
         }
     }
-   
+
 }
--- src/Controllers/Redirect.cs
+++ src/Controllers/Redirect.cs
@@ -1,54 +1,45 @@
+using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using UrlShortener.Models;
-using System.Text.RegularExpressions;
 
-namespace UrlShortener.Controllers
-{
+namespace UrlShortener.Controllers {
 
-    [Route("Redirect/{*redirectAddress}")]
+    [Route ("Redirect/{*redirectAddress}")]
     [ApiController]
-    public class Redirect : Controller
-    {
-         
-         private readonly AppDbContext _appDbContext;
+    public class Redirect : Controller {
 
-         public Redirect(AppDbContext app)
-         {
-             _appDbContext=app;
-         }
-        
+        private readonly AppDbContext _appDbContext;
+
+        public Redirect (AppDbContext app) {
+            _appDbContext = app;
+        }
 
         [HttpGet]
-        public ActionResult Get(string redirectAddress)
-        {
-            string regex=@"^[A-Za-z]+$";
-            Regex r = new Regex(regex); 
-            Url url=new Url();
-            if(redirectAddress.Length !=8)
-            {
-                return BadRequest();
-            }
-            
-            if(!r.IsMatch(redirectAddress))
-            {
-                return BadRequest();
+        public ActionResult Get (string redirectAddress) {
+            string regex = @"^[A-Za-z]+$";
+            Regex r = new Regex (regex);
+            Url url = new Url ();
+            if (redirectAddress.Length != 8) {
+                return BadRequest ();
             }
 
-            if(_appDbContext.Find(url.GetType(),redirectAddress)  !=null)
-            {
-                 url=(Url)_appDbContext.Find(url.GetType(),redirectAddress);
-
-                return Redirect(url.LongUrl);
+            if (!r.IsMatch (redirectAddress)) {
+                return BadRequest ();
             }
-            else
-            {
-                return NotFound();
+
+            if (_appDbContext.Find (url.GetType (), redirectAddress) != null) {
+                
+                url = (Url) _appDbContext.Find (url.GetType (), redirectAddress);
+
+                return Redirect (url.LongUrl);
+            } else {
+                return NotFound ();
             }
         }
-       
-}
+
+    }
 }
